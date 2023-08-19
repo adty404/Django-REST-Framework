4b45a38c69a33194bc8aa1e376032560931dbe28
Aditya Prasetyo <adty404@gmail.com>
2023-08-19 23:09:14 +0700

    7 - Echo GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,20 @@
+import json
 from django.http import JsonResponse
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    print(request.GET) # url query params
+    print(request.POST) # form data
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # string of JSON data -> Python Dict
+    except:
+        pass
+
+    # request.headers is a special Django object = request.META
+    # Convert request.headers to a standard Python dictionary for JSON serialization
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data) # HTTP Response
