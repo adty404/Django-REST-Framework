7936cd5fab943ed3888a87b275c701f8ebb42fc9
Aditya Prasetyo <adty404@gmail.com>
2023-08-20 11:19:18 +0700

    8 - Django Model Instance as API Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,17 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    print(request.GET) # url query params
-    print(request.POST) # form data
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # string of JSON data -> Python Dict
-    except:
-        pass
-
-    # request.headers is a special Django object = request.META
-    # Convert request.headers to a standard Python dictionary for JSON serialization
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        # model instance (model_data)
+        # turn a Python dict
+        # return JSON to my client
     return JsonResponse(data) # HTTP Response
