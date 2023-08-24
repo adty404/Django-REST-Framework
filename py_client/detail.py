c41572030977f1b4f9cf2465f47c12422d37fcc1
Aditya Prasetyo <adty404@gmail.com>
2023-08-24 21:33:45 +0700

    18 - Mixins and a Generic API View

--- py_client/detail.py
+++ py_client/detail.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://localhost:8000/api/products/7/"
+endpoint = "http://localhost:8000/api/products/6/"
 
 get_response = requests.get(endpoint) # HTTP Request
 print(get_response.json())
